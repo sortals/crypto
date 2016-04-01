b4e122acb600eac2299f0b7f1f70678717ccc053
root <root@localhost.localdomain>
2016-03-31 21:52:38 -0400

    Adding in a class for user

--- python/passman.py
+++ python/passman.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
-# TODO: fix writing password to file after multiple iterations of random generation
-#       fix salting
-# 	use os.path for file handling
+''' TODO:
+        authenticate() prompts for password, create user, change user
+            crypt function?
+                
+        fix writing password to file after multiple iterations of random generation
+        make default settings without cli args
+        make option for hashing single strings
+        get scrypt working
+        get option to input new encrypting or hashing functions
+        use os.path for file handling
+'''
 import scrypt
 import sys, argparse 
 import random
@@ -10,8 +18,30 @@ from base64 import b64encode
 from base64 import b64decode
 import bcrypt
 #import sqlite3
-
-
+class User():
+    def __init__(self, username, pass_phrase):
+        self.username = username
+        self.pass_phrase = pass_phrase 
+    def new_user():
+        username_entry = input('Enter username: ')
+        if len(username) < 1:
+            print('Invalid entry')
+            new_user()
+        pass_phrase_entry = input('Enter passphrase: ')
+        if len(pass_phrase) < 1:
+            print('Invalid entry')
+            new_user()
+    def authenticate():
+        username = input('Enter username: ')
+        if len(username) < 1:
+            print('Invalid entry')
+            authenticate()
+        pass_phrase = input('Enter passphrase: ')
+        if len(pass_phrase) < 1:
+            print('Invalid entry')
+            authenticate()
+                
+    
 def generate_pw(chars, length):
     password = ''.join(random.choice(chars) for n in range(length))
     print(password)
@@ -40,12 +70,6 @@ def new(location):
     for i in password_types:
         if int(pt) == password_types.index(i) + 1:
             pass_type = i
-            
-    #elif pt == '1':
-    #    pt = letters_numbers
-    #else:
-    #    print('Invalid selection')
-    #    new()
     r = input('Enter number of symbols (int): ')
     try:
         r = int(r)
@@ -57,8 +81,8 @@ def new(location):
 
 def save(data, location):
     '''I would like this eventually to be modular so that users can include new hash libraries
-       but without risk of over the shoulder attack. For now only bcrypt and scrypt are available
-       and are hard coded user options.'''
+    but without risk of over the shoulder attack. For now only bcrypt and scrypt are available
+    and are hard coded user options.'''
     funcs = ['bcrypt_encrypt', 'scrypt']
     for i in funcs:
         print(funcs.index(i) + 1, i)
@@ -70,13 +94,12 @@ def save(data, location):
         #elif int(alg) == alg_choice[i]:
             #scrypt_hash(data)
 
-def scan(scan_file):
+def scan(location):
     '''This should read from a file the address, username, and password 
        as arbitrarily separated, e.g. comma, newline, tab '''
-    #scan_file = input('Enter file: ')
     data = []
     try:
-        with open(scan_file, 'r') as f:
+        with open(location, 'r') as f:
             for line in f:
                 if line == '\n':
                     pass
@@ -84,7 +107,8 @@ def scan(scan_file):
                     data.append(line)
     except IOError:
         print('File not found')
-    save(data)
+    location = input('Enter save file location: ')
+    save(data, location)
 
 def scrypt_hash(password):
     salt = b64encode(os.urandom(64))
@@ -128,8 +152,6 @@ def bcrypt_hash(password):
     except TypeError:
         print('Input not integer, try again.')
         bcrypt_hash(password) 
-    #int(log)
-    #encoded_password = b64encode(password)
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt(log))
     print('Storing password...')
     with open('secret/bcrypt_test', 'ab') as f:
@@ -141,23 +163,30 @@ def bcrypt_verify(data):
     try:
         with open(data, 'rb') as f:
             for i in f:
-                print(i) 
+                i = i.strip(b'\n') 
                 if bcrypt.hashpw(attempt.encode(), i) == i:
                     matches.append(i)
-        if len(matches) > 0:
-            print('Password accepted')
-        else: 
-            print('Password denied')
-    
+                    print('Match found: ', attempt)
+                else:
+                    print('Failed...')
+        print(matches)
     except IOError:
         print('File not found')                            
     
 def main():
+'''
+
+1 start command line
+2 parse args, opts
+3 prompt for auth or to create user
+
+'''
     print("""
                 PassMan
         
         Usage: python passman.py <options> <arguments>
-        Options: 'new': generate and hash new password\n""") 
+        Options: 'new': generate and hash new password\n""")
+    user_status = User.authenticate()
     for arg in sys.argv:
         if arg == '-n':
             new(sys.argv[sys.argv.index(arg) + 1])
